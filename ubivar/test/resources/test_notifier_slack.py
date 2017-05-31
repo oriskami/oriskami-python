67d92fa5dd794e6117f77bd813712f6134d74c18
Fabrice Colas <fcolas@ubivar.com>
2017-05-31 18:12:58 +0200

    add functional tests for notifier

---
+++ ubivar/test/resources/test_notifier_slack.py
@@ -0,0 +1,57 @@
+import os
+import ubivar
+import warnings
+from ubivar.test.helper import (UbivarTestCase, NOW)
+
+
+DUMMY_SLACK = {
+        "description": "DUMMY SLACK",
+        "feature": "slack_domain",
+        "is_active": "true",
+        "value": "http://a-slack-url"
+        }
+
+
+class UbivarAPIResourcesTests(UbivarTestCase):
+
+    def test_notifier_slack_create(self):
+        response = ubivar.NotifierSlack.create(**DUMMY_SLACK)
+        notifierSlack = response.data[len(response.data)-1]
+        self.assertTrue(hasattr(response, "data"))
+        self.assertEqual(notifierSlack["description"], DUMMY_SLACK["description"])
+        self.assertEqual(notifierSlack["feature"]    , DUMMY_SLACK["feature"])
+        self.assertEqual(notifierSlack["value"]      , DUMMY_SLACK["value"])
+        self.assertEqual(notifierSlack["is_active"]  , DUMMY_SLACK["is_active"])
+        self.assertEqual(response.object, "notifier_slack")
+
+    def test_notifier_slack_list(self):
+        response = ubivar.NotifierSlack.list()
+        self.assertTrue(hasattr(response.data, "__iter__"))
+        self.assertTrue(response.object == "notifier_slack")
+
+    def test_notifier_slack_update(self):
+        response = ubivar.NotifierSlack.list()
+        for i in range(0, len(response.data)):
+            response = ubivar.NotifierSlack.delete(str(i))
+        response = ubivar.NotifierSlack.create(**DUMMY_SLACK)
+        response = ubivar.NotifierSlack.create(**DUMMY_SLACK)
+
+        newDescription = "new description"
+        newValue = "new value"
+        newStatus= "false"
+        newFeature = "new feature"
+        response = ubivar.NotifierSlack.update("1", description=newDescription, 
+                                                 value=newValue, is_active=newStatus,
+                                                 feature=newFeature)
+        notifierSlack = response.data[len(response.data) - 1]
+        self.assertEqual(notifierSlack["description"], newDescription)
+        self.assertEqual(notifierSlack["value"], newValue)
+        self.assertEqual(notifierSlack["is_active"], newStatus)
+        self.assertEqual(notifierSlack["feature"], newFeature)
+        self.assertEqual(response.object, "notifier_slack")
+
+    def test_notifier_slack_delete(self):
+        response = ubivar.NotifierSlack.list()
+        for i in range(0, len(response.data)):
+            response = ubivar.NotifierSlack.delete(str(i))
+            self.assertEqual(response.object, "notifier_slack")
