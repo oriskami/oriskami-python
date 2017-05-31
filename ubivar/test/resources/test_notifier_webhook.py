936d356690278b208f7f42d9a3aee9702e26a8be
Fabrice Colas <fcolas@ubivar.com>
2017-05-31 18:08:57 +0200

    add functional test for the webhook notifier

---
+++ ubivar/test/resources/test_notifier_webhook.py
@@ -0,0 +1,57 @@
+import os
+import ubivar
+import warnings
+from ubivar.test.helper import (UbivarTestCase, NOW)
+
+
+DUMMY_WEBHOOK = {
+        "description": "DUMMY WEBHOOK",
+        "feature": "webhook_domain",
+        "is_active": "true",
+        "value": "http://a-webhook-url"
+        }
+
+
+class UbivarAPIResourcesTests(UbivarTestCase):
+
+    def test_notifier_webhook_create(self):
+        response = ubivar.NotifierWebhook.create(**DUMMY_WEBHOOK)
+        notifierWebhook = response.data[len(response.data)-1]
+        self.assertTrue(hasattr(response, "data"))
+        self.assertEqual(notifierWebhook["description"], DUMMY_WEBHOOK["description"])
+        self.assertEqual(notifierWebhook["feature"]    , DUMMY_WEBHOOK["feature"])
+        self.assertEqual(notifierWebhook["value"]      , DUMMY_WEBHOOK["value"])
+        self.assertEqual(notifierWebhook["is_active"]  , DUMMY_WEBHOOK["is_active"])
+        self.assertEqual(response.object, "notifier_webhooks")
+
+    def test_notifier_webhook_list(self):
+        response = ubivar.NotifierWebhook.list()
+        self.assertTrue(hasattr(response.data, "__iter__"))
+        self.assertTrue(response.object == "notifier_webhooks")
+
+    def test_notifier_webhook_update(self):
+        response = ubivar.NotifierWebhook.list()
+        for i in range(0, len(response.data)):
+            response = ubivar.NotifierWebhook.delete(str(i))
+        response = ubivar.NotifierWebhook.create(**DUMMY_WEBHOOK)
+        response = ubivar.NotifierWebhook.create(**DUMMY_WEBHOOK)
+
+        newDescription = "new description"
+        newValue = "new value"
+        newStatus= "false"
+        newFeature = "new feature"
+        response = ubivar.NotifierWebhook.update("1", description=newDescription, 
+                                                 value=newValue, is_active=newStatus,
+                                                 feature=newFeature)
+        notifierWebhook = response.data[len(response.data) - 1]
+        self.assertEqual(notifierWebhook["description"], newDescription)
+        self.assertEqual(notifierWebhook["value"], newValue)
+        self.assertEqual(notifierWebhook["is_active"], newStatus)
+        self.assertEqual(notifierWebhook["feature"], newFeature)
+        self.assertEqual(response.object, "notifier_webhooks")
+
+    def test_notifier_webhook_delete(self):
+        response = ubivar.NotifierWebhook.list()
+        for i in range(0, len(response.data)):
+            response = ubivar.NotifierWebhook.delete(str(i))
+            self.assertEqual(response.object, "notifier_webhooks")
